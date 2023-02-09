39ccd6911f6d94926952862a7b8d36e0ecd402a6
Thomas McBoyle <tom.mcboyle@gmail.com>
2023-02-09 15:31:54 +0000

    Tests

---
+++ test/test_move_generation.py
@@ -0,0 +1,11 @@
+""" Move generation unit tests """
+import unittest
+from chess.board import Board
+
+# class TestMoveGeneration(unittest.TestCase):
+#     def test_starting_position(self):
+#         board = Board()
+#         moves = board.get_legal_moves()
+#         print([str(m) for m in moves])
+        
+#         self.assertEqual(len(moves), 20)
